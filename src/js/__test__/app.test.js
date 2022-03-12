7509a9d04490c650c1c2ab3edb391bb4f8b041eb
Pavel Fokin <fokinpavel2007@outlook.com>
2022-03-12 09:54:19 +0300

    first commit

---
+++ src/js/__test__/app.test.js
@@ -0,0 +1,13 @@
+﻿import ArrayBufferConverter from '../app.js';
+
+test('Строка', () => {
+  const buffer = new ArrayBufferConverter();
+  buffer.load('Строка текста');
+  expect(buffer.toString()).toBe('Строка текста');
+});
+
+test('Объект в строке', () => {
+  const buffer = new ArrayBufferConverter();
+  buffer.load('{"data":{"user":{"id":1,"name":"Hitman","level":10}}}');
+  expect(buffer.toString()).toBe('{"data":{"user":{"id":1,"name":"Hitman","level":10}}}');
+});
---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+﻿import getBuffer from "./getBuffer";
+
+export default class ArrayBufferConverter {
+    load(buffer){
+       this.buffer = getBuffer(buffer);
+    }
+
+    toString() {
+       return String.fromCharCode(... new Uint16Array(this.buffer));
+    }
+}
